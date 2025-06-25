9c8aa044cc555397c4c196ea98ca277b5497b7a7
ashuk-11 <ashutosh110805@gmail.com>
2025-06-25 10:39:05 +0530

    Router & favico n setup

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,22 +6,24 @@ export default function TextForm(props) {
         // console.log("Uppercase was clicked: " +  text);
         let newText = text.toUpperCase();
         setText(newText)
-        props.showAlert("converted to Uppercase!!", "success");
+        props.showAlert("Converted to uppercase!", "success");
     }
 
     const handleLoClick = ()=>{ 
         let newText = text.toLowerCase();
         setText(newText)
+        props.showAlert("Converted to lowercase!", "success");
     }
 
     const handleClearClick = ()=>{ 
         let newText = '';
-        setText(newText)
+        setText(newText);
+        props.showAlert("Text Cleared!", "success");
     }
 
     const handleOnChange = (event)=>{
         // console.log("On change");
-        setText(event.target.value)
+        setText(event.target.value) 
     }
 
     // Credits: A
@@ -31,12 +33,14 @@ export default function TextForm(props) {
         text.select();
         text.setSelectionRange(0, 9999);
         navigator.clipboard.writeText(text.value);
+        props.showAlert("Copied to Clipboard!", "success");
     }
 
     // Credits: Coding Wala
     const handleExtraSpaces = () => {
         let newText = text.split(/[ ]+/);
-        setText(newText.join(" "))
+        setText(newText.join(" "));
+        props.showAlert("Extra spaces removed!", "success");
     }
 
     const [text, setText] = useState(''); 
@@ -44,10 +48,10 @@ export default function TextForm(props) {
     // setText("new text"); // Correct way to change the state
     return (
         <>
-        <div className="container" style={{color: props.mode==='dark'?'white':'black'}}> 
+        <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}> 
             <h1>{props.heading}</h1>
             <div className="mb-3"> 
-            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#042743':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
@@ -55,7 +59,7 @@ export default function TextForm(props) {
             <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
             <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
-        <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
+        <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h2>Your text summary</h2>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{0.008 *  text.split(" ").length} Minutes read</p>
